8de26bd4c2ca3ba49b869f6d2a5a1a85d437fec8
Allan Rodkin <allan@norwindinteractive.com>
2026-01-26 06:39:22 -0600

    CRUD completed

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,4 +1,7 @@
 using System.Diagnostics;
+using Application.Activities.Commands;
+using Application.Activities.Queries;
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -7,26 +10,38 @@ using Persistence;
 namespace API.Controllers
 {
     
-    public class ActivitiesController(AppDbContext context) : BaseAPIController
+    public class ActivitiesController : BaseAPIController
     {
         [HttpGet]
         public async Task<ActionResult<List<Domain.Activity>>> GetActivities()
         {
-            return await context.Activities.ToListAsync();
+            return await Mediator.Send(new GetActivityList.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Domain.Activity>> GetActivityDetail(string id)
         {
-            var activity = await context.Activities.FindAsync(id);
-            
-            if (activity == null)
-                return NotFound();
-            else
-                return activity;
-            
-            
+            return await Mediator.Send(new GetActivityDetails.Query{Id = id});
+        }
 
+        [HttpPost]
+        public async Task<ActionResult<string>> CreateActivity(Domain.Activity activity)
+        {
+            return await Mediator.Send(new CreateActivity.Command{Activity = activity});
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> EditActivity(Domain.Activity activity)
+        {
+            await Mediator.Send(new EditActivity.Command{Activity = activity});
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteActivity(string id)
+        {
+            await Mediator.Send(new DeleteActivity.Command{Id = id});
+            return Ok();
         }
 
     }
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Application.Activities.Queries;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -11,14 +13,15 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")) ;
 });
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-
-
+builder.Services.AddCors();
+builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-
 app.UseAuthorization();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://localhost:3000"));
+
 
 app.MapControllers();
 
